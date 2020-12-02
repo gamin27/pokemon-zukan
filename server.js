758d5cbdfed07df19b15fc389f1f6ee94b01a38a
gamine <48608531+gamin-yojo@users.noreply.github.com>
2020-12-02 19:07:01 +0900

    heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/docs"));
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
