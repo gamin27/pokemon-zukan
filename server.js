8d694d6805b9d99a861fcd9853fb0cb284834230
gamine <48608531+gamin-yojo@users.noreply.github.com>
2020-12-02 19:21:58 +0900

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/docs"));
